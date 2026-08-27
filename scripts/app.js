399ecb0c6dff2950c0ec09b202a59caf6b6e8767
paytaku <ayana16371212@gmail.com>
2026-08-27 20:48:18 +0900

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,5 @@
 
-window.GA_MEASUREMENT_ID = ""; // ← ここにG-XXXXXXXXXXを入れると有効化
+window.GA_MEASUREMENT_ID = "https://paytaku-ai-import.ayana16371212.workers.dev"; // ← ここにG-XXXXXXXXXXを入れると有効化
 if(window.GA_MEASUREMENT_ID){
   var s = document.createElement("script");
   s.async = true;
